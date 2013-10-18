befa4ac3f2776cc60f06a93edd8c2ed00592a02f
Anass RACH <anas.rach@gmail.com>
2013-10-18 16:59:48 +0200

    Update app.js
    accès aux ressources static public

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname + '/public')));
+app.use(express.static(path.join(__dirname + 'public')));
  
 app.get('/', function(req, res) {
     
